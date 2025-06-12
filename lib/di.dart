70486a2b5d966b366fc2d154d650e1b6646e33e1
putra <putra@web8.is>
2025-06-12 17:26:59 +0700

    Refactor: Modularize dependency injection
    This commit refactors the dependency injection setup by:
    
    - Separating the core and authentication module dependencies into their respective locator functions (`_coreLocator` and `_authLocator`).
    - Initializing these locators within the main `setup` function.
    - Changing the `hiveBoxName` for `DioCacheInterceptor` from 'qurani' to 'local'.
    
    This improves code organization and maintainability by grouping related dependencies.

--- lib/di.dart
+++ lib/di.dart
@@ -3,19 +3,26 @@ import 'package:dio_cache_interceptor/dio_cache_interceptor.dart';
 import 'package:dio_cache_interceptor_hive_store/dio_cache_interceptor_hive_store.dart';
 import 'package:get_it/get_it.dart';
 import 'package:intl/date_symbol_data_local.dart';
-import 'package:path_provider/path_provider.dart';
 import 'package:my_boilerplate/core/core.dart';
+import 'package:my_boilerplate/features/auth/auth.dart';
+import 'package:my_boilerplate/features/auth/data/datasource/auth_datasource.dart';
+import 'package:path_provider/path_provider.dart';
 
 final di = GetIt.instance;
 
 Future<void> setup() async {
   initializeDateFormatting();
 
+  _coreLocator();
+  _authLocator();
+}
+
+void _coreLocator() async {
   var dir = await getTemporaryDirectory();
 
   /* -----------------> Network <-----------------*/
   di.registerFactory<Dio>(
-        () => Dio(
+    () => Dio(
       BaseOptions(
         connectTimeout: const Duration(seconds: 35),
         receiveTimeout: const Duration(seconds: 35),
@@ -25,23 +32,50 @@ Future<void> setup() async {
     instanceName: "interceptor",
   );
   di.registerFactory<Dio>(
-        () => Dio(
-      BaseOptions(
-        connectTimeout: const Duration(seconds: 35),
-        receiveTimeout: const Duration(seconds: 35),
-        sendTimeout: const Duration(seconds: 35),
-      ),
-    )..interceptors.addAll([
-      NetworkInterceptor(),
-      DioCacheInterceptor(
-        options: CacheOptions(
-          store: HiveCacheStore(dir.path, hiveBoxName: 'qurani'),
-          priority: CachePriority.high,
-          policy: CachePolicy.forceCache,
-          maxStale: const Duration(days: 7),
-          hitCacheOnErrorExcept: [],
-        ),
-      ),
-    ]),
+    () =>
+        Dio(
+            BaseOptions(
+              connectTimeout: const Duration(seconds: 35),
+              receiveTimeout: const Duration(seconds: 35),
+              sendTimeout: const Duration(seconds: 35),
+            ),
+          )
+          ..interceptors.addAll([
+            NetworkInterceptor(),
+            DioCacheInterceptor(
+              options: CacheOptions(
+                store: HiveCacheStore(dir.path, hiveBoxName: 'local'),
+                priority: CachePriority.high,
+                policy: CachePolicy.forceCache,
+                maxStale: const Duration(days: 7),
+                hitCacheOnErrorExcept: [],
+              ),
+            ),
+          ]),
+  );
+}
+
+void _authLocator() {
+  di.registerLazySingleton<AuthDataSource>(
+    () => AuthDataSourceImpl(dioClient: di<NetworkClient>()),
+  );
+
+  di.registerLazySingleton<AuthRepository>(
+    () => AuthRepositoryImpl(dataSource: di<AuthDataSource>()),
+  );
+
+  di.registerLazySingleton<LoginUsecase>(
+    () => LoginUsecase(repository: di<AuthRepository>()),
+  );
+
+  di.registerLazySingleton<RegisterUsecase>(
+    () => RegisterUsecase(repository: di<AuthRepository>()),
+  );
+
+  di.registerFactory<LoginBloc>(
+    () => LoginBloc(loginUsecase: di<LoginUsecase>()),
+  );
+  di.registerFactory<RegisterBloc>(
+    () => RegisterBloc(registerUsecase: di<RegisterUsecase>()),
   );
 }
