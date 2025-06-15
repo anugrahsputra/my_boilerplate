a5465cf8676122c769441e3c1ff0c1b99210e618
anugrahsputra <anugrahsputra@gmail.com>
2025-06-15 18:51:32 +0700

    Feat: Add UnauthorizedFailure type
    This commit introduces a new `UnauthorizedFailure` class, which extends the base `Failure` class.
    
    This new failure type can be used to specifically represent situations where an operation fails due to lack of authorization.

--- lib/core/domain/failures.dart
+++ lib/core/domain/failures.dart
@@ -13,6 +13,10 @@ class ServerFailure extends Failure {
   const ServerFailure({required super.message});
 }
 
+class UnauthorizedFailure extends Failure {
+  const UnauthorizedFailure({required super.message});
+}
+
 class CacheFailure extends Failure {
   const CacheFailure({required super.message});
 }
