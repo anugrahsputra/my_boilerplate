3868a575759313cdfc54117a545f9e3a1156c1af
anugrahsputra <anugrahsputra@gmail.com>
2025-06-15 18:52:51 +0700

    Refactor: Utilize `safeCall` for error handling in `AuthRepositoryImpl`
    This commit refactors the `AuthRepositoryImpl` to use a `safeCall` utility function for handling potential exceptions during API calls.
    
    Previously, `login` and `register` methods used individual try-catch blocks to handle `ServerException`, `NetworkException`, `NotFoundException`, and `UnknownException`.
    
    The `safeCall` function encapsulates this error handling logic, making the repository methods more concise and readable. It now wraps the datasource calls and mapping operations.

--- lib/features/auth/data/repository/auth_repository_impl.dart
+++ lib/features/auth/data/repository/auth_repository_impl.dart
@@ -1,4 +1,5 @@
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 import 'package:my_boilerplate/core/core.dart';
 import 'package:my_boilerplate/features/auth/auth.dart';
 import 'package:my_boilerplate/features/auth/data/datasource/auth_datasource.dart';
@@ -10,33 +11,17 @@ class AuthRepositoryImpl implements AuthRepository {
 
   @override
   Future<Either<Failure, Login>> login(LoginReqDto loginReq) async {
-    try {
+    return safeCall(() async {
       final result = await dataSource.login(loginReq);
-      return Right(result.toLogin());
-    } on ServerException {
-      return const Left(ServerFailure(message: 'Server Failure'));
-    } on NetworkException {
-      return const Left(NetworkFailure(message: "No Internet Connection"));
-    } on NotFoundException {
-      return const Left(RequestFailure(message: "Not Found"));
-    } on UnknownException {
-      return const Left(UnknownFailure(message: "Unknown Error"));
-    }
+      return result.toLogin();
+    });
   }
 
   @override
   Future<Either<Failure, Register>> register(RegisterReqDto registerReq) async {
-    try {
+    return safeCall(() async {
       final result = await dataSource.register(registerReq);
-      return Right(result.toRegister());
-    } on ServerException {
-      return const Left(ServerFailure(message: 'Server Failure'));
-    } on NetworkException {
-      return const Left(NetworkFailure(message: "No Internet Connection"));
-    } on NotFoundException {
-      return const Left(RequestFailure(message: "Not Found"));
-    } on UnknownException {
-      return const Left(UnknownFailure(message: "Unknown Error"));
-    }
+      return result.toRegister();
+    });
   }
 }
