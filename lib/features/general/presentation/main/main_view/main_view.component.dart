e2280753e0d9b782164d6aeef2d8a5d91c38206f
anugrahsputra <anugrahsputra@gmail.com>
2025-06-23 16:58:28 +0700

    Feat: Add initial MainView structure
    This commit introduces the basic structure for the `MainView` feature:
    
    - **`MainView` Widget:**
        - Creates a `StatefulWidget` named `MainView`.
        - Includes a `Scaffold` with an `AppBar` titled "Main View".
    - **Component File:**
        - Adds a `main_view.component.dart` file as part of `main_view.dart`.
    - **Barrel Files:**
        - Creates barrel files (`main.dart`, `presentation.dart`, `general.dart`) to export the new `MainView` and its related directories (data, domain, presentation) for the "general" feature.
    - **Empty Directories:**
        - Adds empty `data.dart` and `domain.dart` files under the `general` feature, presumably as placeholders for future development.

---
+++ lib/features/general/presentation/main/main_view/main_view.component.dart
@@ -0,0 +1,1 @@
+part of 'main_view.dart';
---
+++ lib/features/general/presentation/main/main_view/main_view.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+part 'main_view.component.dart';
+
+class MainView extends StatefulWidget {
+  const MainView({super.key});
+
+  @override
+  State<MainView> createState() => _MainViewState();
+}
+
+class _MainViewState extends State<MainView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Main View"),
+      ),
+    );
+  }
+}
