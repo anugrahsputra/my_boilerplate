208e7e00524478c89fda0b19ae623668c606b123
anugrahsputra <anugrahsputra@gmail.com>
2025-09-06 02:38:57 +0700

    feat: Add initial project setup script
    This commit introduces a `setup.sh` script to automate initial project configuration.
    
    The script performs the following actions:
    1. Prompts the user to enter the desired app package name and app name.
    2. Uses the `rename` pub global package to:
        - Set the bundle ID for both Android and iOS.
        - Set the app name for both Android and iOS.
    3. Runs `flutter clean` and `flutter pub get` to prepare the project.

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo "🚀 Setting up your Flutter project..."
+
+# 1. Rename app
+read -r -p "Enter your app package name (e.g., com.example.myapp): " package
+read -r -p "Enter your app name: " appname
+
+# 2. Change package name automatically
+flutter pub global activate rename
+flutter pub global run rename setBundleId --targets android,ios --bundleId "$package"
+
+# 3. Change app name automatically
+flutter pub global run rename setAppName --targets android,ios --appname "$appname"
+
+# 4. Clean & get dependencies
+flutter clean
+flutter pub get
+
+echo "✅ Setup complete! You're ready to code 🚀"
