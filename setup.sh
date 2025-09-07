a911413f7eb0db172ae1a259c6809c1fcaba8387
anugrahsputra <anugrahsputra@gmail.com>
2025-09-07 20:16:09 +0700

    feat: Add project name change to setup script
    This commit updates the `setup.sh` script to include a step for changing the project name.
    
    - It prompts the user to enter their desired project name.
    - It activates and runs the `change_project_name` package to apply the new name.
    - The subsequent "Clean & get dependencies" step has been renumbered.

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ echo "🚀 Setting up your Flutter project..."
 # 1. Rename app
 read -r -p "Enter your app package name (e.g., com.example.myapp): " package
 read -r -p "Enter your app name: " appname
+read -r -p "Enter your project name: " projectname
 
 # 2. Change package name automatically
 flutter pub global activate rename
@@ -12,7 +13,11 @@ flutter pub global run rename setBundleId --targets android,ios --value  "$packa
 # 3. Change app name automatically
 flutter pub global run rename setAppName --targets android,ios --value  "$appname"
 
-# 4. Clean & get dependencies
+# 4. Change project name automatically
+flutter pub global activate change_project_name
+flutter pub global run change_project_name "$projectname"
+
+# 5. Clean & get dependencies
 flutter clean
 flutter pub get
 
